fcc376d271a8196d32b4a2044aa5e7d053c76b72
jmerilai-oulu <jmerilai19@univ.yo.oulu.fi>
2023-08-07 00:07:40 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("I am from master branch!");
+    first_feature();
+    second_feature();
+}
+
+fn first_feature() {
+    println!("I am from a dev branch!");
+}
+
+fn second_feature() {
+    println!("I am from a dev branch too!");
+}
